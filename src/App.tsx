ba31a2ac55d15a949b4fd207b7fd1231f67834d4
Your Name <you@example.com>
2026-01-20 11:59:19 +0530

    "theme"

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { ThemeProvider } from "./contexts/ThemeContext";
+
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="dark" storageKey="wakanda-theme">
       <TooltipProvider>
         <Toaster />
         <Sonner />
@@ -23,7 +22,7 @@ const App = () => (
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
-    </ThemeProvider>
+    
   </QueryClientProvider>
 );
 
